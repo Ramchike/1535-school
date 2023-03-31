6f8704d9219e750f526c6fd4d00f9fe906edd891
Ramchik <rrramirrr@yandex.ru>
2023-03-31 22:46:34 +0300

    real work in school exam

---
+++ 1535 real work/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<vector<int>> arr = {};
+    int a, b, c, n;
+    cin >> a >> b >> c >> n;
+    int combo = 0;
+
+    for (int a1 = 0; a1 < 10; a1++) {
+            for (int b1 = 0; b1 < 10; b1++) {
+                for (int c1 = 0; c1 < 10; c1++) {
+                    if (a * a1 + b * b1 + c * c1 == n) {
+                        combo++;
+                        arr.push_back(vector<int> {a1, b1, c1});
+                    }
+                }
+            }
+    }
+
+    cout << combo << endl;
+
+    for (const auto& oops: arr) {
+        for (int go: oops) {
+            cout << go << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
