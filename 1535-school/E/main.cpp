260cd14047c491c8bffc0b67e2cef1ea8d27fc1a
Ramchik <rrramirrr@yandex.ru>
2023-04-01 19:54:51 +0300

    num3 update

--- 1535-school/E/main.cpp
+++ 1535-school/E/main.cpp
@@ -14,7 +14,7 @@ int main() {
 
     for (int i = 0; i < n; i++)
         cin >> p[i];
-
+    
     for (int i = 0; i < n; i++)
         cin >> t[i];
 
---
+++ hse-school/num3/num3.cpp
@@ -0,0 +1,39 @@
+﻿#include "iostream"
+#include "string"
+
+using namespace std;
+
+string SearchSW(string sen, char symbol) {
+	string copy = sen;
+	string valid, trash;
+	int min = INT_MAX;
+
+	for (char c : copy) {
+		if (isspace(c)) {
+			if (trash[trash.length() - 1] == symbol) {
+				if (trash.length() < min) {
+					valid = trash;
+					trash.clear();
+					min = valid.length();
+				}
+				else
+					trash.clear();
+			}
+		}
+		else
+			trash.push_back(c);
+	}
+
+	return valid;
+}
+
+int main()
+{
+	string sentense;
+	getline(cin, sentense);
+
+	sentense.erase(sentense.find(SearchSW(sentense, 'e')), SearchSW(sentense, 'e').length() + 1);
+
+	cout << sentense;
+	return 0;
+}
