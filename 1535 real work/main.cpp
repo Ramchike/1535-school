0af573926d62639677a4e758bcb253acf635715c
Ramchik <rrramirrr@yandex.ru>
2023-04-01 17:52:58 +0300

    update real work

--- 1535 real work/main.cpp
+++ 1535 real work/main.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 int main() {
+    vector<vector<int>> arr;
 
-    vector<vector<int>> arr = {};
     int a, b, c, n;
     cin >> a >> b >> c >> n;
+
     int combo = 0;
 
     for (int a1 = 0; a1 < 10; a1++) {
-            for (int b1 = 0; b1 < 10; b1++) {
-                for (int c1 = 0; c1 < 10; c1++) {
+        for (int b1 = 0; b1 < 10; b1++) {
+            for (int c1 = 0; c1 < 10; c1++) {
                     if (a * a1 + b * b1 + c * c1 == n) {
                         combo++;
                         arr.push_back(vector<int> {a1, b1, c1});
-                    }
                 }
             }
+        }
     }
 
     cout << combo << endl;
