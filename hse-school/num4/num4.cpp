187f3fe7f04d8630c89b7203fc0d66e0547a519a
Ramchik <rrramirrr@yandex.ru>
2023-04-01 20:54:13 +0300

    num4

---
+++ hse-school/num4/num4.cpp
@@ -0,0 +1,42 @@
+﻿// num4.cpp: определяет точку входа для приложения.
+//
+
+#include "num4.h"
+
+using namespace std;
+
+int main()
+{
+	int max = INT_MIN;
+	int c_max = 0, sum = 0;
+
+	vector<int> arr (20);
+
+	for (int i = 0; i < 20; i++) {
+		cin >> arr[i];
+		if (arr[i] > max) {
+			max = arr[i];
+			c_max = i + 1;
+		}
+	}
+
+	for (int i : arr) {
+		if (i == max) {
+			break;
+		}
+		else if (i > 0) {
+			sum += i;
+		}
+	}
+
+
+	for (int i = 0; i < 19; i++) {
+			if (c_max == i){
+				cout << sum << " ";
+			}
+		else
+			cout << arr[i] << " ";
+	}
+
+	return 0;
+}
---
+++ hse-school/num4/num4/num4.cpp
@@ -0,0 +1,12 @@
+﻿// num4.cpp: определяет точку входа для приложения.
+//
+
+#include "num4.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
