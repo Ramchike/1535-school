8840df92a2758ca99241eafd9ee2ffa5dbf43c95
Ramchik <rrramirrr@yandex.ru>
2023-04-10 13:19:27 +0300

    add num5

---
+++ 2107-school/num5/num5.cpp
@@ -0,0 +1,22 @@
+﻿// num5.cpp: определяет точку входа для приложения.
+//
+
+#include "num5.h"
+
+using namespace std;
+
+int pl_min(vector<int> arr) {
+	int count = 0;
+	for (int i = 0; i < arr.size() - 1; i++) {
+		if (arr[i] > 0 && arr[i + 1] < 0 || arr[i] < 0 && arr[i + 1] > 0)
+			count++;
+	}
+	return count;
+}
+
+int main()
+{
+	vector<int> arr = {10, -4, 12, 56, -4, -89};
+	cout << pl_min(arr);
+	return 0;
+}
