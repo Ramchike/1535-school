b64bb5617dd5076ebef9188a58889e4d720bc9be
Ramchik <rrramirrr@yandex.ru>
2023-04-10 11:27:59 +0300

    add 2107

---
+++ 2107-school/D/D.cpp
@@ -0,0 +1,34 @@
+﻿// D.cpp: определяет точку входа для приложения.
+//
+
+#include "D.h"
+
+using namespace std;
+
+int main()
+{
+
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	int l_max = 1, max = 1;
+
+	for (int i = 0; i < n - 1; i++) { // проход по массиву
+
+		if (arr[i + 1] > arr[i]) { // проверка на возрастание
+			l_max++;
+		}
+		else if (l_max > max) {
+			max = l_max;
+			l_max = 1;
+		}
+		else l_max = 1;	
+	}
+	cout << max << endl;
+	return 0;
+}
---
+++ olymp/gazprom/Num2/Num2.cpp
@@ -0,0 +1,17 @@
+﻿// Num2.cpp: определяет точку входа для приложения.
+//
+
+#include "Num2.h"
+
+using namespace std;
+
+int main()
+{
+	double a, b; // первый, второй участок
+	cin >> a >> b;
+	double g = pow(a, b);
+
+	cout << endl << int(g) % 10;
+
+	return 0;
+}
