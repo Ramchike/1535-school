52bb9b0c33a7b25e35ee05e778f4e50f19cd1177
Ramchik <rrramirrr@yandex.ru>
2023-04-10 11:47:04 +0300

    add num2

---
+++ 2107-school/num2/num2.cpp
@@ -0,0 +1,21 @@
+﻿// num2.cpp: определяет точку входа для приложения.
+//
+
+#include "num2.h"
+
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	swap(s[0], s[1]);
+	swap(s[2], s[3]);
+
+	if (s[0] == '0')
+		s.erase(0, 1);
+
+	cout << s;
+	return 0;
+}
