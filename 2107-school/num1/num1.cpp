32b73e6e9567e621d8f5cb676b52b05119621438
Ramchik <rrramirrr@yandex.ru>
2023-04-10 13:05:48 +0300

    add num1

---
+++ 2107-school/num1/num1.cpp
@@ -0,0 +1,17 @@
+﻿// num1.cpp: определяет точку входа для приложения.
+//
+
+#include "num1.h"
+
+using namespace std;
+
+double func(double x, double y) {
+	return pow(x, 3) - 2.5 * x * y + 1.78 * pow(x, 2) - 2.5 * y + 1;
+}
+int main()
+{
+	double x, y;
+	cin >> x >> y;
+	cout << func(x, y);
+	return 0;
+}
