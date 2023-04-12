198baf0f5c3d1adb0dcd6fb5924c2f39d62bc37d
Ramchik <rrramirrr@yandex.ru>
2023-04-12 20:59:48 +0300

    add simple codes

---
+++ simple-codes/max_length-string/max_length-string.cpp
@@ -0,0 +1,27 @@
+﻿// max_length-string.cpp: определяет точку входа для приложения.
+//
+
+#include "max_length-string.h"
+
+using namespace std;
+
+int main()
+{
+	string enter, max_s = "", s;
+	getline(cin, enter);
+	enter.push_back(' ');
+
+	for (char c : enter) {
+		if (isspace(c)) {
+			max_s = (max_s.length() < s.length()) ? s : max_s;
+			s.clear();
+		}
+		else {
+			s.push_back(c);
+		}
+
+	}
+
+	cout << max_s << endl << max_s.length();
+	return 0;
+}
---
+++ simple-codes/palindrom-string/palindrom-string.cpp
@@ -0,0 +1,12 @@
+﻿// palindrom-string.cpp: определяет точку входа для приложения.
+//
+
+#include "palindrom-string.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
